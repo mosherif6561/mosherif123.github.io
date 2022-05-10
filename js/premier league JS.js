7e47bf9eec74f4fa43faaa44968aa636c357a1b2
Mohamed Sherif <93544780+mosherif6561@users.noreply.github.com>
2022-05-10 16:13:22 +0200

    Update premier league JS.js

--- js/premier league JS.js
+++ js/premier league JS.js
@@ -1,5 +1,5 @@
 //on click go the top of the page (scroll up)
-function topFunction() {
+function scrollupFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
  
@@ -15,4 +15,31 @@ darkMode.addEventListener("click",function(){
     
 })();
 
+// button salah 
+function salah() {
+  var x = document.getElementById("moh");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
 
+// button trezeguet
+function trezeguet() {
+  var x = document.getElementById("tre");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
+// button elneny
+function elneny() {
+  var x = document.getElementById("neno");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+};
